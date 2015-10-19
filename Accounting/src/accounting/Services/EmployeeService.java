2f3160ce74883f3fb663c919d9a37ce01c7b1e00
Vasiliy Kirstia <kirstiavasilii@gmail.com>
2015-10-19 17:37:03 +0300

    добавил реализацию сервиса сотрудников

--- Accounting/src/accounting/Services/EmployeeService.java
+++ Accounting/src/accounting/Services/EmployeeService.java
@@ -6,8 +6,13 @@
 package accounting.Services;
 
 import accounting.Interfaces.IEmployeeService;
+import accounting.Models.Currency;
 import accounting.Models.Employee;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -18,17 +23,33 @@ public class EmployeeService implements IEmployeeService{
 
     @Override
     public void addEmployee(Connection con, String employeeName) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement prepSt = con.prepareStatement("INSERT INTO employee (name) VALUES (?)");
+        prepSt.setString(1, employeeName);
+        prepSt.executeUpdate();
     }
 
     @Override
     public Employee getEmployeeById(Connection con, int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement prepSt = con.prepareStatement("SELECT * FROM employee WHERE id = ?");
+        prepSt.setInt(1, id);
+        ResultSet resultSet = prepSt.executeQuery();
+        
+        if(resultSet.next()){
+            return new Employee(resultSet.getInt("id"), resultSet.getString("name"));
+        }else{
+            return null;
+        }
     }
 
     @Override
     public List<Employee> getAllEmployees(Connection con) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Statement st = con.createStatement();
+        ResultSet resultSet = st.executeQuery("SELECT * FROM employee");
+        List<Employee> employees = new LinkedList<Employee>();
+        while(resultSet.next()){
+            employees.add(new Employee(resultSet.getInt("id"), resultSet.getString("name")));
+        }
+        return employees;
     }
 
 }
