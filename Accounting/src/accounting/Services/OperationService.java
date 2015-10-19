02e87f73607f809081dd953c86eaca850807b09c
Vasiliy Kirstia <kirstiavasilii@gmail.com>
2015-10-19 17:44:59 +0300

    добавил реализацию сервиса операций

--- Accounting/src/accounting/Services/OperationService.java
+++ Accounting/src/accounting/Services/OperationService.java
@@ -10,6 +10,7 @@ import accounting.Models.Operation;
 import java.util.List;
 
 import java.sql.*;
+import java.util.LinkedList;
 
 /**
  *
@@ -19,17 +20,33 @@ public class OperationService implements IOperationService{
 
     @Override
     public void addOperation(Connection con, String operationName) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement prepSt = con.prepareStatement("INSERT INTO operation (name) VALUES (?)");
+        prepSt.setString(1, operationName);
+        prepSt.executeUpdate();
     }
 
     @Override
     public Operation getOperationById(Connection con, int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement prepSt = con.prepareStatement("SELECT * FROM operation WHERE id = ?");
+        prepSt.setInt(1, id);
+        ResultSet resultSet = prepSt.executeQuery();
+        
+        if(resultSet.next()){
+            return new Operation(resultSet.getInt("id"), resultSet.getString("name") );
+        }else{
+            return null;
+        }
     }
 
     @Override
     public List<Operation> getAllOperations(Connection con) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Statement st = con.createStatement();
+        ResultSet resultSet = st.executeQuery("SELECT * FROM operation");
+        List<Operation> operations = new LinkedList<Operation>();
+        while(resultSet.next()){
+            operations.add(new Operation(resultSet.getInt("id"), resultSet.getString("name")));
+        }
+        return operations;
     }
     
 }
