93e5459e064ee8b4c5c2b54f1f1ef74e59a261a3
Vasiliy Kirstia <kirstiavasilii@gmail.com>
2015-11-19 19:17:54 +0300

    закончил скрипт для создания таблиц

--- database.sql
+++ database.sql
@@ -20,12 +20,42 @@ CREATE TABLE document(
 
 CREATE TABLE employee(
 	id SERIAL PRIMARY KEY,
-	name TEXT
+	name TEXT NOT NULL
 );
 
 CREATE TABLE operation(
 	id SERIAL PRIMARY KEY,
-	name CHARACTER VARYING(10)
+	name CHARACTER VARYING(10) NOT NULL
 );
 
-CREATE OTHER TABLE ... 
+CREATE TABLE product_unit(
+	id SERIAL PRIMARY KEY,
+	name CHARACTER VARYING(10) NOT NULL
+);
+
+CREATE TABLE product_group(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	account_id INTEGER REFERENCES account (id)
+);
+
+CREATE TABLE product(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	amount DOUBLE PRECISION CHECK ( 0 <= amount ),
+	price NUMERIC(15, 3) CHECK ( 0 < price ),
+	currency_id INTEGER REFERENCES currency (id),
+	product_group_id INTEGER REFERENCES product_group (id),
+	product_unit_id INTEGER REFERENCES product_unit (id)
+);
+
+CREATE TABLE transaction(
+	id SERIAL PRIMARY KEY,
+	date DATE NOT NULL,
+	product_amount DOUBLE PRECISION CHECK ( 0 < product_amount ),
+	operation_id INTEGER REFERENCES operation ( id ),
+	product_id INTEGER REFERENCES product ( id ),
+	destination_id INTEGER REFERENCES destination ( id ),
+	employee_id INTEGER REFERENCES employee ( id ),
+	document_id INTEGER REFERENCES document ( id )
+);
